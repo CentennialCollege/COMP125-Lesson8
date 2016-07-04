5580cb0f05d5bd3c92e0caf86398111baea14004
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-07-04 09:23:58 -0400

    demo'd math.random

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,6 +13,29 @@
 (function () {
     "use strict";
 
-   
+    // references to HTMLElements 
+    var rollButton = document.getElementById("rollButton");
+    var diceResult = document.getElementById("diceResult");
+
+    function init() {
+        console.log("Application Started");
+
+    }
+
+    function diceRoll() {
+        var die1 = Math.floor((Math.random() * 6) + 1);
+        var die2 = Math.floor((Math.random() * 6) + 1);
+        var total = die1 + die2;
+        diceResult.innerHTML = "<h2>" + total + "</h2>";
+    }
+
+    rollButton.addEventListener("click", diceRoll);
+
+
+
+
+    // call init funciton when window finishes loading
+    window.addEventListener("load", init);
+
 
 })();
